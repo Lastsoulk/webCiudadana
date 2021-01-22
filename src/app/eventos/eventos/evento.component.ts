b5ea8c6893d2d550313951b8345f5c9c86f1223c
Luis Montenegro <lfmonten@espol.edu.ec>
2021-01-22 03:32:07 -0500

    3 primeras tareas del txt terminadas

--- src/app/eventos/eventos/evento.component.ts
+++ src/app/eventos/eventos/evento.component.ts
@@ -7,6 +7,7 @@ import { FireBaseService, ICampaña } from '../../services/fire-base.service';
 import { NavigationExtras, Router } from '@angular/router';
 import { AngularFirestore } from "@angular/fire/firestore";
 import { MatTableDataSource } from '@angular/material/table';
+import firebase from 'firebase';
 
 /**
  * @title Card with multiple sections
@@ -29,8 +30,9 @@ export class Eventos {
 
   public events = [];
   public categories = [];
+  public dates = [];
 
-  public tipoEvento = ["Todas","Convocatorias","Noticias"];
+  public tipoEvento = ["Todas", "Convocatorias", "Noticias"];
   public ciudades = ["Todas"];
   public esConvocatoria = false;
   public condicioneventovacio = false;
@@ -40,7 +42,7 @@ export class Eventos {
     private firestore: AngularFirestore,
     private firestoreService: FireBaseService,
     public router: Router,
-    
+
   ) {
 
   }
@@ -63,80 +65,119 @@ export class Eventos {
   }
 
 
-  getEvents(ciudad,tipo): void {
-    if(tipo=='Convocatorias'){
+  getEvents(ciudad, tipo): void {
+    if (tipo == 'Convocatorias') {
       this.esConvocatoria = true;
     }
-    if(tipo=='Noticias'){
+    if (tipo == 'Noticias') {
       this.esConvocatoria = false;
     }
-    this.firestoreService.getEvents(ciudad,tipo).subscribe((eventsSnapshot) => {
+    this.firestoreService.getEvents(ciudad, tipo).subscribe((eventsSnapshot) => {
       this.events = [];
       this.categories = [];
-      console.log('veamos: ',eventsSnapshot.length)
+      let i = 0
+      console.log('veamos: ', eventsSnapshot.length)
       eventsSnapshot.forEach((event: any) => {
-        this.events.push({
-          name: event.payload.doc.data().name,
-          address: event.payload.doc.data().address,
-          description: event.payload.doc.data().description,
-          eventId: event.payload.doc.id,
-          eventPic: event.payload.doc.data().eventPic,
-          state: event.payload.doc.data().state,
-          dateEvent: event.payload.doc.data().dateEvent,
-          city: event.payload.doc.data().city,
+        //console.log('test', event.payload.doc.data())
+        if (new Date(event.payload.doc.data().dateEvent).getTime() > new Date().getTime() && event.payload.doc.data().type === "convocatoria") {
+          console.log('La fecha del evento es menor al now', new Date(event.payload.doc.data().dateEvent).getTime() > new Date().getTime())
+          this.events.push({
+            name: event.payload.doc.data().name,
+            address: event.payload.doc.data().address,
+            description: event.payload.doc.data().description,
+            eventId: event.payload.doc.id,
+            eventPic: event.payload.doc.data().eventPic,
+            state: event.payload.doc.data().state,
+            dateEvent: event.payload.doc.data().dateEvent,
+            city: event.payload.doc.data().city,
+            type: event.payload.doc.data().type
+          });
 
-        });
+          //console.log('campanaid', event.payload.doc.data().campaignId)
+          var refproduct = firebase.firestore();
+          refproduct.collection('campaigns').doc(event.payload.doc.data().campaignId).get().then(snapshot => {
+            //console.log('campana', snapshot.data().name)
+            this.events[i]['nombrecampana'] = snapshot.data().name
+            i++
+          })
+        } else if (event.payload.doc.data().type == 'noticia') {
+          this.events.push({
+            name: event.payload.doc.data().name,
+            address: event.payload.doc.data().address,
+            description: event.payload.doc.data().description,
+            eventId: event.payload.doc.id,
+            eventPic: event.payload.doc.data().eventPic,
+            state: event.payload.doc.data().state,
+            dateEvent: event.payload.doc.data().dateEvent,
+            city: event.payload.doc.data().city,
+            type: event.payload.doc.data().type
+          });
+
+          //console.log('campanaid', event.payload.doc.data().campaignId)
+          var refproduct = firebase.firestore();
+          refproduct.collection('campaigns').doc(event.payload.doc.data().campaignId).get().then(snapshot => {
+            //console.log('campana', snapshot.data().name)
+            this.events[i]['nombrecampana'] = snapshot.data().name
+            i++
+          })
+
+        }
       });
-      console.log("this.events", this.events.length);
-      
+      console.log("this.events", this.events);
+      this.events.sort(this.sortFunction);
+      console.log('ordenado', this.events)
+
       if (this.events.length == 0) {
-          this.condicioneventovacio = true;
+        this.condicioneventovacio = true;
       } else {
-          this.condicioneventovacio = false;
+        this.condicioneventovacio = false;
       }
       // this.dataSource.data = this.campaigns as Campaign[];
     }, (error) => {
       console.log("Error al cargar los eventos", error)
     });
 
-    
+
   }
+
+  sortFunction(a, b) {
+    var dateA = new Date(a.dateEvent).getTime();
+    var dateB = new Date(b.dateEvent).getTime();
+    return dateA < dateB ? 1 : -1;
+  };
 
 
   ngOnInit(): void {
 
 
-    
-    this.getCiudades();
-    this.getEvents("Todas","Todas");
 
+    this.getCiudades();
+    this.getEvents("Todas", "Todas");
 
   }
 
-  getCiudades(){
+  getCiudades() {
 
-        
+
 
     console.log('Aqui--')
-        this.firestoreService.getCiudades().subscribe((ciudadesSnapshot) => {
-        //  this.ciudades = [];
-          ciudadesSnapshot.forEach((ciudades: any) => {
-            var elemento = ciudades.payload.doc.data().city
-            if(this.ciudades.includes(elemento)){
-                
-            }else{
-                const add = this.ciudades.push(elemento)
-            }
-           
+    this.firestoreService.getCiudades().subscribe((ciudadesSnapshot) => {
+      //  this.ciudades = [];
+      ciudadesSnapshot.forEach((ciudades: any) => {
+        var elemento = ciudades.payload.doc.data().city
+        if (this.ciudades.includes(elemento)) {
+
+        } else {
+          const add = this.ciudades.push(elemento)
+        }
+
       });
 
-    
+
     })
-    
+
 
   }
-
-
 
 
 
